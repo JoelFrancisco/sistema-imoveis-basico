76759b36171350c38d1ceff0c8a379dde64e5b1f
viniciusmilanez <vinicius.254711@alunosatc.edu.br>
2023-06-13 21:48:31 -0300

    Estilizando page relatorios

--- front/src/pages/Reports.jsx
+++ front/src/pages/Reports.jsx
@@ -2,10 +2,6 @@
 
 // Param data_inicio e data_fim - relacionadas ao pagamento/vencimentos
 
-// nome_imobiliária; qtde_imoveis_licados; Valor_total_locacao; Qtde_imoveis_locacao_aberto; Valor_total_locacao_aberto.
-
-// Trazer o valor total da divida.
-
 import {
   Flex,
   Button,
@@ -24,21 +20,42 @@ import {
 
 export default function Reports() {
   return (
-    <Flex as="main" flexDirection="column" padding="8" justifyContent="center">
-      <FormControl>
+    <Flex
+      as="main"
+      flexDirection="column"
+      padding="8"
+      justifyContent="center"
+      alignItems="center"
+      gap="1rem"
+    >
+      <FormControl
+        as="form"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
         <ButtonGroup>
           <Button>Ranking imobiliaria</Button>
           <Button>Ranking dividendos</Button>
         </ButtonGroup>
       </FormControl>
       <TableContainer>
-        <Table variant="simple" bgColor="lightgray">
-          <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <Table
+          variant="simple"
+          size="sm"
+          bgColor="lightgray"
+          borderRadius="10"
+          fontFamily="verdana"
+        >
+          <TableCaption>Relatorio - Ranking Imobiliarias</TableCaption>
           <Thead>
             <Tr>
               <Th>nome imobiliaria</Th>
-              <Th>data inicio</Th>
-              <Th>data fim</Th>
+              <Th>imoveis locados</Th>
+              <Th>total contratos</Th>
+              <Th>total contratos abertos</Th>
+              {/* <Th>data inicio</Th>
+              <Th>data fim</Th> */}
               <Th>contratos vencidos</Th>
               <Th>total dividendos</Th>
             </Tr>
@@ -46,22 +63,31 @@ export default function Reports() {
           <Tbody>
             <Tr>
               <Td>Imobiliária Prime</Td>
-              <Td>15/03/2022</Td>
-              <Td>14/03/2023</Td>
+              <Td>5</Td>
+              <Td>5</Td>
+              <Td>2</Td>
+              {/* <Td>15/03/2022</Td>
+              <Td>14/03/2023</Td> */}
               <Td>3</Td>
               <Td>20.000,00</Td>
             </Tr>
             <Tr>
               <Td>Nova Era Imobiliária</Td>
-              <Td>10/09/2022</Td>
-              <Td>14/03/2023</Td>
+              <Td>5</Td>
+              <Td>5</Td>
+              <Td>2</Td>
+              {/* <Td>10/09/2022</Td>
+              <Td>14/03/2023</Td> */}
               <Td>1</Td>
               <Td>10.000,00</Td>
             </Tr>
             <Tr>
               <Td>Casa & Lar Imóveis</Td>
-              <Td>10/09/2022</Td>
-              <Td>31/12/2022</Td>
+              <Td>5</Td>
+              <Td>5</Td>
+              <Td>2</Td>
+              {/* <Td>10/09/2022</Td>
+              <Td>31/12/2022</Td> */}
               <Td>1</Td>
               <Td>6.000,00</Td>
             </Tr>
@@ -69,8 +95,11 @@ export default function Reports() {
           <Tfoot>
             <Tr>
               <Th>nome imobiliaria</Th>
-              <Th>data inicio</Th>
-              <Th>data fim</Th>
+              <Th>imoveis locados</Th>
+              <Th>total contratos</Th>
+              <Th>total contratos abertos</Th>
+              {/* <Th>data inicio</Th>
+              <Th>data fim</Th> */}
               <Th>contratos vencidos</Th>
               <Th>total dividendos</Th>
             </Tr>
