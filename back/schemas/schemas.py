371bafd50f48821ef287c9fb24c8924187532be6
lucaspaixao-code <lucassernajoto8@gmail.com>
2023-06-20 19:21:52 -0300

    ajustes_api

--- back/schemas/schemas.py
+++ back/schemas/schemas.py
@@ -1,31 +1,14 @@
 from pydantic import BaseModel
 
-
-# Schemas para Pessoa
-class PessoaBase(BaseModel):
+class PessoaSchema(BaseModel):
+    cd_pessoa: int
     nome: str
     telefone: str
     dt_nascimento: str
     cpf: str
 
-
-class PessoaCreate(PessoaBase):
-    pass
-
-
-class PessoaUpdate(PessoaBase):
-    pass
-
-
-class Pessoa(PessoaBase):
-    cd_pessoa: int
-
-    class Config:
-        orm_mode = True
-
-
-# Schemas para Contrato
-class ContratoBase(BaseModel):
+class ContratoSchema(BaseModel):
+    cd_contrato: int
     valor: int
     data_inicio: str
     data_fim: str
@@ -34,66 +17,59 @@ class ContratoBase(BaseModel):
     cd_locatario: int
     cd_imovel: int
 
-
-class ContratoCreate(ContratoBase):
-    pass
-
-
-class ContratoUpdate(ContratoBase):
-    pass
-
-
-class Contrato(ContratoBase):
-    cd_contrato: int
-
-    class Config:
-        orm_mode = True
-
-
-# Schemas para Pagamento
-class PagamentoBase(BaseModel):
-    valor_pago: float
+class PagamentoSchema(BaseModel):
+    cd_pagamento: int
+    valor_pa: float
     data_pagamento: str
     valor_trasferido: float
     data_vencimento: str
     email: str
     status_pagamento: str
+    cd_contrato: int
 
-
-class PagamentoCreate(PagamentoBase):
-    pass
-
-
-class PagamentoUpdate(PagamentoBase):
-    pass
-
-
-class Pagamento(PagamentoBase):
-    cd_pagamento: int
-
-    class Config:
-        orm_mode = True
-
-
-# Schemas para Imovel
-class ImovelBase(BaseModel):
+class ImovelSchema(BaseModel):
+    cd_imovel: int
     ds_imovel: str
     tipo_imovel: str
     area_construida: float
     area_total: float
     cd_endereco: int
 
+class DadosBancariosSchema(BaseModel):
+    cd_dadosbancarios: int
+    banco_locatario: str
+    agencia_locatario: int
+    conta_locatario: int
+    cd_pessoa: int
 
-class ImovelCreate(ImovelBase):
-    pass
+class ImobiliariaSchema(BaseModel):
+    cd_imobiliaria: int
+    nome_locadora: str
+    cnpj: str
+    email: str
 
+class EnderecoSchema(BaseModel):
+    cd_endereco: int
+    rua: str
+    numero: int
+    cep: str
+    cd_bairro: int
 
-class ImovelUpdate(ImovelBase):
-    pass
+class BairroSchema(BaseModel):
+    cd_bairro: int
+    nome_bairro: str
+    cd_cidade: int
 
+class CidadeSchema(BaseModel):
+    cd_cidade: int
+    nome_cidade: str
+    uf: str
 
-class Imovel(ImovelBase):
-    cd_imovel: int
+class EstadoSchema(BaseModel):
+    uf: str
+    nome_estado: str
+    cd_pais: int
 
-    class Config:
-        orm_mode = True
+class PaisSchema(BaseModel):
+    cd_pais: int
+    nome_pais: str
--- back/server/database.py
+++ back/server/database.py
@@ -1,10 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import urllib
 
-DATABASE_URL = "sqlite:///./sqlite.db"
+server = 'nome-do-servidor-do-banco.database.windows.net'
+database = 'nome-do-banco-de-dados'
+username = 'lucas1234'
+password = 'Adm-1234'
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+params = urllib.parse.quote_plus(
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    'SERVER=' + server + ';'
+    'DATABASE=' + database + ';'
+    'UID=' + username + ';'
+    'PWD=' + password + ';'
+)
+
+engine = create_engine('jdbc:sqlserver://;serverName=nome-do-servidor-do-banco.database.windows.net;databaseName=nome-do-banco-de-dados')
+SessionLocal = sessionmaker(bind=engine)
 
 def get_db():
     db = SessionLocal()
