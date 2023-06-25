be089f4d0b677e0d6cf19bf8b23f97c6f95b36b8
João Victor Miotelli Vitali <119806814+JoaoMiotelli@users.noreply.github.com>
2023-06-24 21:52:07 -0300

    Update SQL_Functions.sql
    Adição das funções feitas pelo Lucas de Oliveira Alano.

--- scripts/SQL_Functions.sql
+++ scripts/SQL_Functions.sql
@@ -1,5 +1,4 @@
 ﻿--Função para calcular valor de não pagantes(inadimplentes)
-
 CREATE OR ALTER FUNCTION fn_valor_inadimplentes_imobiliaria(@cd_imobiliaria int) RETURNS INT
 AS
 BEGIN
@@ -20,7 +19,6 @@ END;
 
 
 --Função de cálculo da quantidade de imobiliárias.
-
 CREATE OR ALTER FUNCTION fn_qnt_imoveis(@cd_imobiliaria int) RETURNS INT
 AS
 BEGIN
@@ -33,4 +31,55 @@ BEGIN
         AND c.status_contrato = 'A'
 
     RETURN @QuantidadeImoveis;
+END;
+
+
+
+-- Funciont valor total mensal da imobiliaria
+CREATE OR ALTER FUNCTION fn_valor_total_imobiliaria(@cd_imobiliaria int) RETURNS INT
+AS
+BEGIN
+    DECLARE @ValorMensalImobiliaria INT;
+
+    SELECT @ValorMensalImobiliaria = SUM(p.valor_trasferido)
+    FROM Contrato c
+    INNER JOIN Pagamento p ON c.cd_contrato = p.cd_contrato
+    INNER JOIN imobiliaria i ON c.cd_imobiliaria = i.cd_imobiliaria
+    WHERE i.cd_imobiliaria = @cd_imobiliaria 
+
+    RETURN @ValorMensalImobiliaria;
+END;
+
+
+
+-- Quantidade total de locações em aberto por imobiliaria 
+CREATE OR ALTER FUNCTION fn_qnt_imoveis_em_aberto(@cd_imobiliaria int) RETURNS INT
+AS
+BEGIN
+    DECLARE @QuantidadeImoveis INT;
+
+    SELECT @QuantidadeImoveis = COUNT(*)
+    FROM Contrato c
+    INNER JOIN imobiliaria i ON c.cd_imobiliaria = i.cd_imobiliaria
+    WHERE i.cd_imobiliaria = @cd_imobiliaria 
+        AND c.status_contrato = 'P'  -- Pendente, no caso ainda sem locatário
+
+    RETURN @QuantidadeImoveis;
+END;
+
+
+-- Função total da locação por imobiliaria em aberto
+CREATE OR ALTER FUNCTION fn_valor_total_em_aberto (@cd_imobiliaria int) RETURNS INT
+AS
+BEGIN
+    DECLARE @ValorEmAberto INT;
+
+    SELECT @ValorEmAberto = SUM(c.valor)
+    FROM Contrato c
+    INNER JOIN imobiliaria i ON c.cd_imobiliaria = i.cd_imobiliaria
+    WHERE i.cd_imobiliaria = @cd_imobiliaria 
+        AND c.status_contrato = 'P'
+
+
+    RETURN @ValorEmAberto;
 END;
