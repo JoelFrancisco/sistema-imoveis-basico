4a0ef81cd2678c09481112d8cf898b95874c9c5c
Castrozan <castro.lucas290@gmail.com>
2023-06-26 20:57:55 -0300

    Feat: adição de um arquivo para chamada com as procedures

---
+++ scripts/ChamadaProcedures.sql
@@ -0,0 +1,17 @@
+EXEC pc_relatorio_valor_imovel;
+
+EXEC pr_relatorio_pagamento_por_locatario;
+
+EXEC pc_locatario_inadimplente;
+
+EXEC pr_valor_inadimplentes;
+
+EXEC pr_ranking_imobiliarias;
+
+EXEC pr_relatorio_pagamento_por_locatario;
+
+EXEC pr_relatorio_qnt_imoveis_locador;
+
+EXEC pr_qnt_imoveis_locatario;
+
+EXEC pr_relatorio_imoveis_regiao;
