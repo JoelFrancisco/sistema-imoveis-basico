4c7e7441cc3f6cd862b1601e387154d20ca705eb
Lucas Alano <74608202+LucasAlano@users.noreply.github.com>
2023-06-27 20:09:23 -0300

    Update ChamadaProcedures.sql
    Atualizando chamada das proc

--- scripts/ChamadaProcedures.sql
+++ scripts/ChamadaProcedures.sql
@@ -5,8 +5,10 @@ EXEC pr_relatorio_pagamento_por_locatario;
 EXEC pc_locatario_inadimplente;
 
 EXEC pr_valor_inadimplentes;
-
-EXEC pr_ranking_imobiliarias;
+SELECT * FROM temp_valor_inadimplente
+  
+EXEC pr_ranking_imobiliarias '2023-1-1', '2023-12-12'
+SELECT * FROM temp_ranking_imobiliarias
 
 EXEC pr_relatorio_pagamento_por_locatario;
 
